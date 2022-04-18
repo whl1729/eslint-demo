bc4e25f28b768d75dc3b22314b9f6224c6b8c930
wuhualong <wuhualong@minieye.cc>
2022-04-18 19:17:51 +0800

    add Airbnb demo: Destructuring

---
+++ airbnb/5_1_object_destructure.js
@@ -0,0 +1,6 @@
+function getFullName(user) {
+  const firstName = user.firstName;
+  const lastName = user.lastName;
+
+  return `${firstName} ${lastName}`;
+}
---
+++ airbnb/5_2_array_destructure.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4];
+
+// bad
+const first = arr[0];
+const second = arr[1];
